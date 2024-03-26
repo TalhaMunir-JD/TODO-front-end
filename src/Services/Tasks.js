b1fa868f33d6c09c223c3512f20862c03191a35c
Talha Munir <talhamunir411@gmail.com>
2024-03-26 14:38:35 +0500

    Made the service of the tasks which initiates the requests from frontend.

---
+++ src/Services/Tasks.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const getAllTasks = () => {
+    return(
+        axios.get('http://localhost:8000/api/get/')
+    )
+}
+
+const createTasks = (obj) => {
+    return(
+        axios.post('http://localhost:8000/api/post/', obj)
+    )
+}
+
+const editTasks = (id, updatedTask) => {
+    return(
+        axios.put(`http://localhost:8000/api/put/${id}`, updatedTask)
+    )
+}
+
+const deleteTask = (id) => {
+    return(
+        axios.delete(`http://localhost:8000/api/delete/${id}`)
+    )
+}
+
+export default{
+    getAllTasks: getAllTasks, 
+    createTasks: createTasks, 
+    editTasks: editTasks, 
+    deleteTask: deleteTask
+}
