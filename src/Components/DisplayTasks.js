d6e5cbf2818437dbc69c958f1d7910865fc27995
Talha Munir <talhamunir411@gmail.com>
2024-04-01 19:07:09 +0500

    Displaying of functionality in the code using pagination.

--- src/Components/DisplayTasks.js
+++ src/Components/DisplayTasks.js
@@ -1,13 +1,18 @@
-import { Table, TableBody, TableCell, TableRow, Button } from "@mui/material";
+import { Table, TableBody, TableCell, TableRow, Button, TableHead, Paper } from "@mui/material";
 import DeleteTask from "./DeleteTask";
 import EditTask from "./EditTasks";
 import { useState } from "react";
 import taskService from '../Services/Tasks';
+import { MdModeEdit } from "react-icons/md";
+import EditName from "./EditName";
+import EditDescription from "./EditDescription";
 
 function DisplayTasks({ tasksList, setTasks }){
 
     const[Editedtaskid, setEditedTaskid] = useState(null)
     const[showDescriptionid, setShowDescriptionid] = useState([])
+    const[editNameid, setEditNameid] = useState(null)
+    const[editDescriptionId, setEditDescriptionId] = useState(null)
 
     const handleTaskClick = (taskid) => {
         let tasks = [...showDescriptionid]
@@ -21,18 +26,20 @@ function DisplayTasks({ tasksList, setTasks }){
         setShowDescriptionid(tasks)
     }
 
+// Handling the task editing here and below
     const handleEditButtonClick = (taskid) => {
         setEditedTaskid(taskid)
-        console.log(taskid)
     }
 
     const handleEditSubmit = (taskId, newName, newDescription) => {
         console.log(newName, newDescription)
+
         const editedTask = {
             id: taskId,
             name: newName,
             description: newDescription
         }
+
         taskService.editTasks(taskId, editedTask).then(response=>{
             console.log("response from edit request: ", response)
             // setTasks(prevTasks=>[...prevTasks.filter(task => task.id !== taskId), response.data])
@@ -56,33 +63,119 @@ function DisplayTasks({ tasksList, setTasks }){
         setEditedTaskid(null)
     }
 
+// Handling the name editing of the task here and below
+    const handlenameedit = (taskid) => {
+        setEditNameid(taskid)
+    }
 
-    
+    const editNameCancel=()=>{
+        setEditNameid(null)
+    }
+
+    const editNameSubmit=(taskId, name)=>{
+        console.log(name, taskId, typeof(taskId))
+        const editedName = {
+            'name': name
+        }
+        taskService.editName(taskId, editedName).then(response=>{
+            console.log("response from the editname request is: ",response)
+            console.log(editedName);
+            const updatedTasksList = tasksList.map(task => {
+                if (task.id === taskId) {
+                    return {...task, 'name': name}
+                } else {
+                    return task;
+                }
+            });
+            // Update state with the updated tasks list
+            setTasks(updatedTasksList);
+            setEditNameid(null)
+        }).catch(error=>{
+
+            console.log("Error occured in edit name request: ", error)
+        })
+        console.log("Name editted successfully.")
+    }
+
+//edit description request here.
+    const handledescriptionedit = (taskid) => {
+        setEditDescriptionId(taskid)
+    }
+
+    const editdescriptionCancel = () => {
+        setEditDescriptionId(null)
+    }
+
+    const editDescriptionSubmit = (taskId, description) => {
+        console.log(description, taskId, typeof(taskId))
+        const editedDescription = {
+            'description': description
+        }
+        taskService.editDescription(taskId, editedDescription).then(response=>{
+            const updatedTasksList = tasksList.map(task => {
+                if (task.id === taskId) {
+                    return {...task, 'description': description}
+                } else {
+                    return task;
+                }
+            });
+            // Update state with the updated tasks list
+            setTasks(updatedTasksList);
+            setEditDescriptionId(null)
+        }).catch(error=>{
+            console.log(error.message)
+        })
+    }
+
     return(
         <div>
             <h1>Tasks</h1>
-            <Table style={{width: '100%', display:'flex', justifyContent: 'center'}}>
+            <Table >
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="center"><strong>Task no</strong></TableCell>
+                        <TableCell align="center"><strong>Task</strong></TableCell>
+                        <TableCell align="center"><strong>Actions</strong></TableCell>
+                    </TableRow>
+                </TableHead>
                 <TableBody>
                     {
                         tasksList && tasksList.length > 0 ?
-                        tasksList.map((task, index)=>(
-                            <TableRow key={index}>
-                                <TableCell align="center" key={index} onClick={()=>handleTaskClick(task.id)} >
-                                    {task.id} {task.name} 
+                        tasksList.map((task, index) => (
+                            <TableRow key={task.id}>
+                                <TableCell>{index = index + 1}</TableCell>
+                                <TableCell align="center" key={index} >
+                                    <span onClick={()=>handleTaskClick(task.id)}>
                                     {
-                                        showDescriptionid.indexOf(task.id) !== -1 && <p>{task.description} </p> 
+                                        editNameid === task.id ?
+                                        (<EditName Task={task} onSubmit={editNameSubmit} onCancel={editNameCancel}/>)
+                                        : (task.name)
+                                    }
+                                    </span>
+                                    {
+                                        editNameid === task.id
+                                        ? null :
+                                        (<MdModeEdit onClick={()=>handlenameedit(task.id)} style={{marginLeft:'5px'}}/>)
+                                    }
+                                    {
+                                        editDescriptionId === task.id ? 
+                                        (<EditDescription Task={task} onSubmit={editDescriptionSubmit} onCancel={editdescriptionCancel}/>)
+                                        : (showDescriptionid.indexOf(task.id) !== -1 && <p>{task.description} <MdModeEdit onClick={()=>handledescriptionedit(task.id)}/></p>)
                                     }
                                 </TableCell>
                                 <TableCell align="center">
                                     {
                                         Editedtaskid === task.id ?
                                         (<EditTask taskitem={task} onsubmit={handleEditSubmit} oncancel={handleEditCancel}/>)
-                                        : (<Button variant="contained" size="small" onClick={()=>handleEditButtonClick(task.id)}>Edit Button</Button>)
-                                    } 
+                                        : (<div style={{display:'flex'}}>
+                                            <Button variant="contained" size="small" onClick={()=>handleEditButtonClick(task.id)}>Edit Task</Button>
+                                            <DeleteTask tasksList={tasksList} taskitem={task} setTasks={setTasks} />
+                                        </div>)
+                                    }
                                 </TableCell>
-                                <TableCell align="center">
+                                {/* <TableCell align="center">
                                     <DeleteTask tasksList={tasksList} taskitem={task} setTasks={setTasks} />
-                                </TableCell>
+                                </TableCell> */}
                             </TableRow>
                         )) : null
                     }
